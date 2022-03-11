cea8269bfabd379ee1294baec19c560b539db240
Vikash Chander <28572584+vikashchander@users.noreply.github.com>
2022-03-11 18:40:13 +0530

    remove: remove navbar links

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -11,30 +11,21 @@ import {
 function NavBar() {
   return (
     <header>
-      <MDBNavbar expand='lg' light bgColor='white'>
+      <MDBNavbar expand="lg" light bgColor="white">
         <MDBContainer fluid>
           <MDBNavbarToggler
-            aria-controls='navbarExample01'
-            aria-expanded='false'
-            aria-label='Toggle navigation'
+            aria-controls="navbarExample01"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
           >
-            <MDBIcon fas icon='bars' />
+            <MDBIcon fas icon="bars" />
           </MDBNavbarToggler>
-          <div className='collapse navbar-collapse' id='navbarExample01'>
-            <MDBNavbarNav right className='mb-2 mb-lg-0'>
+          <div className="collapse navbar-collapse" id="navbarExample01">
+            <MDBNavbarNav right className="mb-2 mb-lg-0">
               <MDBNavbarItem active>
-                <MDBNavbarLink aria-current='page' href='#'>
-                  Home
+                <MDBNavbarLink aria-current="page" href="#">
+                  Pizza App
                 </MDBNavbarLink>
-              </MDBNavbarItem>
-              <MDBNavbarItem>
-                <MDBNavbarLink href='#'>Features</MDBNavbarLink>
-              </MDBNavbarItem>
-              <MDBNavbarItem>
-                <MDBNavbarLink href='#'>Pricing</MDBNavbarLink>
-              </MDBNavbarItem>
-              <MDBNavbarItem>
-                <MDBNavbarLink href='#'>About</MDBNavbarLink>
               </MDBNavbarItem>
             </MDBNavbarNav>
           </div>
